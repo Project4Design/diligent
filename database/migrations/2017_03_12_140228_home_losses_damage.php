341a79dae38a6dd8d4cce566fc8c739b9e15a1f2
francisco Hernandez <francisco20990@gmail.com>
2017-03-12 10:12:30 -0400

    relations new
    Relations home-insurance & home_damage

---
+++ database/migrations/2017_03_12_140228_home_losses_damage.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HomeLossesDamage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //FORM YOUR INSURANCE HISTORY , EL QUE SE ENCUENTRA EN HOME FORM INSURANCE
+        Schema::create('home_losses_damage', function (Blueprint $table) {
+            $table->increments('id_home_losses_damage');
+            $table->integer('home_insurance_id')->unsigned();
+            $table->foreign('home_insurance_id')->references('id_home_insurance')->on('home_insurance')->onDelete('cascade');
+            $table->string('much_was_claim')->nullable();
+            $table->string('type_claim')->nullable();
+            $table->string('date_claim')->nullable();
+            $table->string('claim_settled')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_losses_damage');
+    }
+}
